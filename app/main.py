67fcd68b6441e00e27049e734da00ad8ea5806a2
Andre Marcio <andre.marcio.ti@gmail.com>
2026-04-10 11:47:51 -0300

    verify task

--- app/main.py
+++ app/main.py
@@ -47,4 +47,3 @@ class FullStackDeveloper(BackendDeveloper,
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
-
